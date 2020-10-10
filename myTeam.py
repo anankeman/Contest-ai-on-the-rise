68e51fa6ee3937b65d806b9243868e9c018ffc20
Felipe <framosmorale@student.edu.au>
2020-10-10 14:20:55 +1100

    Attack n defend integration

--- myTeam.py
+++ myTeam.py
@@ -24,7 +24,7 @@ import math
 #################
 
 def createTeam(firstIndex, secondIndex, isRed,
-                             first = 'AttackAgent', second = 'defenseAgent'):
+                             first = 'AttackAgent', second = 'DeffendAgent'):
     """
     This function should return a list of two agents that will form the
     team, initialized using firstIndex and secondIndex as their agent
@@ -47,93 +47,6 @@ def createTeam(firstIndex, secondIndex, isRed,
 # Agents #
 ##########
 
-class defenseAgent(CaptureAgent):
-    
-    def registerInitialState(self, gameState):
-        self.start = gameState.getAgentPosition(self.index)
-        
-        CaptureAgent.registerInitialState(self, gameState)
-        self.frontier = halfGrid(gameState.data.layout, self.red, gameState)
-        print(self.frontier)
-        
-        
-    def chooseAction(self, gameState):
-        start = time.time()
-        myPos = gameState.getAgentState(self.index).getPosition()
-        actions = gameState.getLegalActions(self.index)
-        
-        enemies = [gameState.getAgentState(i) for i in self.getOpponents(gameState)]
-        pac = [a for a in enemies if a.isPacman and a.getPosition() != None]
-        #amIpacman = gameState.getAgentState(self.index).isPacman
-        
-        if len(pac)>0:
-            nextAction = self.goForPacman(gameState, pac, actions, myPos)
-        else:
-            nextAction = self.patrol(gameState, myPos, actions)
-            
-        print('eval time for agent %d: %.4f' % (self.index, time.time() - start))
-        
-        return nextAction
-    
-    def goForPacman(self, gameState, pacman, actions, myPos):
-        pacmanPos = pacman[0].getPosition()
-        #distanceToFront = {i: self.getMazeDistance(myPos, i) for i in self.frontier}
-        #nextGoal = min(distanceToFront, key= lambda k: distanceToFront[k])
-        
-        bestDist = 999999
-        for action in actions:
-            successor = self.getSuccessor(gameState, action)
-            pos2 = successor.getAgentPosition(self.index)
-            dist = self.getMazeDistance(pos2, pacmanPos)
-            if dist < bestDist:# and ghostDistance > ghostFear:
-              bestAction = action
-              bestDist = dist
-        nextAction = bestAction
-        
-        return nextAction
-      
-    
-    def getSuccessor(self, gameState, action):
-        
-        successor = gameState.generateSuccessor(self.index, action)
-        pos = successor.getAgentState(self.index).getPosition()
-        if pos != util.nearestPoint(pos):
-          # Only half a grid position was covered
-          return successor.generateSuccessor(self.index, action)
-        else:
-          return successor
-        
-        
-        return successor
-    
-    def patrol(self, gameState, myPos, actions):
-        
-        distanceToFront = {i: self.getMazeDistance(myPos, i) for i in self.frontier}
-        nextGoal = min(distanceToFront, key= lambda k: distanceToFront[k])
-        bestDist = 999999
-        for action in actions:
-            successor = self.getSuccessor(gameState, action)
-            pos2 = successor.getAgentPosition(self.index)
-            dist = self.getMazeDistance(pos2, nextGoal)
-            if dist < bestDist:# and ghostDistance > ghostFear:
-              bestAction = action
-              bestDist = dist
-        nextAction = bestAction
-        
-        return nextAction
-        
-
-def halfGrid(grid, red, gameState):
-    border = [0]*grid.height
-    if red:
-        x = (grid.width//2)-1
-    else:
-        x= (grid.width//2)
-    for y in range(grid.height):
-        if not gameState.hasWall(x, y):
-            border[y] = (x, y)
-    border= [i for i in border if i!=0]
-    return border
 
 #-------------------------------------------------------------------------------------
 class AttackAgent(CaptureAgent):
@@ -367,3 +280,245 @@ class AttackAgent(CaptureAgent):
 
 #-----------------------------------------------------------------------------------
 
+class DeffendAgent(AttackAgent):
+    
+    def registerInitialState(self, gameState):
+        
+        CaptureAgent.registerInitialState(self, gameState)
+        self.count = 0
+        self.target = None
+        self.boundaries = self.getBoundaries(gameState, gameState.data.layout, self.red)
+        food_list = self.getFoodYouAreDefending(gameState).asList()
+        self.initial_food = len(food_list)
+        self.centerOurFood = self.getCenterOurFood(gameState)
+        self.scared = gameState.getAgentState(self.index).scaredTimer
+        #from here are the patrol instance variables
+        self.top = self.getTop(gameState)
+        self.bottom = self.getBottom(gameState)
+        self.goingUp = False
+
+    def chooseAction(self, gameState):
+        start = time.time()
+        self.scared = gameState.getAgentState(self.index).scaredTimer
+        pos = gameState.getAgentPosition(self.index)
+        self.centerOurFood = self.getCenterOurFood(gameState)
+        
+        width_x = gameState.data.layout.height #this is half y, not half x
+        halfway = width_x//2
+        previous = self.getPreviousObservation()
+        food_list_previous = []
+        if previous is not None:
+            food_list_previous = self.getFoodYouAreDefending(previous).asList()
+            if previous.hasFood(pos[0], pos[1]):
+                self.count += 1
+            elif (self.red & pos[0] < halfway) or (not self.red & pos[0] > halfway):
+                self.count = 0
+        
+    
+        food_list_current = self.getFoodYouAreDefending(gameState).asList()
+        if self.target is not None: 
+            
+            
+            
+            ############
+            
+            
+            if pos == self.target:
+                #next two if statements added for patrol
+                if self.target == self.top:
+                    self.goingUp = False
+                elif self.target == self.bottom:
+                    self.goingUp = True
+                # ----------------------
+                self.target = None
+
+        invaders = self.getExactInvaders(gameState, pos)
+        if invaders is not None:
+            self.target = invaders
+            if self.scared == 0:
+                path = self.aStarSearch(gameState, 'getInvaders')
+            else:
+                path = self.aStarSearch(gameState, 'getInvaders_scared', start)
+        elif len(food_list_previous) - len(food_list_current) > 0:
+            self.target = list(set(food_list_previous) - set(food_list_current))[0]
+            path = self.aStarSearch(gameState, 'getFood')
+        elif self.target is not None:
+            path = self.aStarSearch(gameState, 'getInvaders')
+        # patrol added below
+        # if no target, goes to the border, and then as long as agent is within manhattan dist of 5 goes on patrol
+        else:
+            border_dist = util.manhattanDistance(self.getNearestBorder(gameState), pos)
+            if border_dist > 5:
+                self.target = self.getNearestBorder(gameState)
+                path = self.aStarSearch(gameState, 'goCenter')
+            else:
+                if self.goingUp == False:
+                    self.target = self.bottom
+                    path = self.aStarSearch(gameState, 'goCenter')
+                else:
+                    self.target = self.top
+                    path = self.aStarSearch(gameState, 'goCenter')
+        # ----------------------------------
+        #else:
+        #    return Directions.STOP
+        print('eval time for agent %d: %.4f' % (self.index, time.time() - start))
+        return path
+    
+    def getTop(self, gameState):
+        height = gameState.data.layout.height
+        if self.red:
+            top = (self.boundaries[0][0] - 2, height - 4)
+        else:
+            top = (self.boundaries[0][0] + 2, height - 4)
+        x, y = top
+        if gameState.hasWall(x, y):
+            for i in range(2):
+                for j in range(2):
+                    next_x = x - 1 + i
+                    next_y = y - 1 + j
+                    if not gameState.hasWall(next_x, next_y):
+                        top = (next_x, next_y)
+        return top
+
+    #gets the bottom coordinate for the patrol. checks 8 further coordinates if wall found.
+    def getBottom(self, gameState):
+        #height = gameState.data.layout.height
+        if self.red:
+            bottom = (self.boundaries[0][0] - 2, 4)
+        else:
+            bottom = (self.boundaries[0][0] + 2, 4)
+        x, y = bottom
+        if gameState.hasWall(x, y):
+            for i in range(2):
+                for j in range(2):
+                    next_x = x - 1 + i
+                    next_y = y - 1 + j
+                    if not gameState.hasWall(next_x, next_y):
+                        bottom = (next_x, next_y)
+        return bottom
+    
+    def getExactInvaders(self, gameState, pos):
+        opponents_pos_list = util.Counter()
+        current_position = pos
+        opponents_index_list = self.getOpponents(gameState)
+        opponents_agent_list = [gameState.getAgentState(x) for x in opponents_index_list]
+
+        for oponent_agent in opponents_agent_list:
+            if oponent_agent.isPacman:
+                opponent_pos = oponent_agent.getPosition()
+                if opponent_pos is not None:
+                    dist = self.getMazeDistance(current_position, opponent_pos)
+                    opponents_pos_list[opponent_pos] = -dist
+        if opponents_pos_list:
+            key = opponents_pos_list.argMax()
+            return key
+        else:
+            return None
+
+
+    #List with opponents classified in PacMan or Ghost, and the Maze Distance to it
+    def getOpponentsDistances(self, gameState, pos):
+
+        opponents_distance_list = util.Counter()
+        opponents_noisyDistance_list = util.Counter()
+        final = {}
+        current_position = pos
+
+        opponents_index_list = self.getOpponents(gameState)
+        opponents_agent_list = [gameState.getAgentState(x) for x in opponents_index_list]
+
+        for oponent_agent in opponents_agent_list:
+            if oponent_agent.isPacman:
+                index = opponents_agent_list.index(oponent_agent)
+                opponent_pos = oponent_agent.getPosition()
+                if opponent_pos is not None:
+                    dist = self.getMazeDistance(current_position, opponent_pos)
+                    opponents_distance_list[opponent_pos] = -dist
+                else:
+                    approx_distances = self.getCurrentObservation().getAgentDistances()[index]
+                    opponents_noisyDistance_list[oponent_agent] = -abs(approx_distances)
+        if len(opponents_distance_list):
+            key = opponents_distance_list.argMax()
+            final['exact'] = [key, opponents_distance_list[key]]
+        elif len(opponents_noisyDistance_list):
+            key = opponents_noisyDistance_list.argMax()
+            final['noisy'] = [key, opponents_noisyDistance_list[key]]
+        else:
+            final = []
+
+        return final
+    
+    def getCenterOurFood(self, gameState):
+        food_list = self.getFoodYouAreDefending(gameState).asList()
+        x_sum = 0
+        y_sum = 0
+        len_list = len(food_list)
+        for x, y in food_list:
+            x_sum += x
+            y_sum += y
+
+        near_pos = nearestPoint((x_sum/len_list, y_sum/len_list))
+        near = 999
+        final_pos = ()
+        for pos_food in food_list:
+            dist = util.manhattanDistance(pos_food, near_pos)
+            if dist < near:
+                near = dist
+                final_pos = pos_food
+        return final_pos
+    
+    def getExactInvader(self, gameState, pos):
+        return self.getMazeDistance(pos, self.target)
+
+    def heuristic_Astar(self, successor, goal):
+        features = util.Counter()
+        pos = successor.getAgentPosition(self.index)
+        if goal == "getInvaders" or goal == "getInvaders_scared":
+            features['minDistanceFood'] = 99
+            features['minDistanceOpponent'] = self.getExactInvader(successor, pos)
+            features['minDistanceCapsule'] = 99
+            features['minDistanceOurArea'] = 99
+        elif goal == "getFood":
+            features['minDistanceFood'] = self.getExactInvader(successor, pos)
+            features['minDistanceOpponent'] = 99
+            features['minDistanceCapsule'] = 99
+            features['minDistanceOurArea'] = 99
+        else: #goal == "goCenter"
+            features['minDistanceFood'] = self.getMazeDistance(pos, self.target)
+            features['minDistanceOpponent'] = 99
+            features['minDistanceCapsule'] = 99
+            features['minDistanceOurArea'] = 99
+        weights = self.getWeights(goal)
+        return features*weights
+
+    def getWeights(self, goal):
+        if goal in ["getInvaders", "getInvaders_scared"]:
+            return {'minDistanceFood': 1,
+                                            'minDistanceOpponent': 1,
+                                            'minDistanceCapsule': 1,
+                                            'minDistanceOurArea': 1}
+        elif goal == "getFood":
+            return {'minDistanceFood': 1,
+                                            'minDistanceOpponent': 1,
+                                            'minDistanceCapsule': 1,
+                                            'minDistanceOurArea': 1}
+        else: #"goCenter"
+            return {'minDistanceFood': 1,
+                                            'minDistanceOpponent': 1,
+                                            'minDistanceCapsule': 1,
+                                            'minDistanceOurArea': 1}
+        
+    def getNearestBorder(self, gameState):
+            current_position = gameState.getAgentPosition(self.index)
+            boundary_distance = 9999
+            boundary_position = None
+            for i in self.boundaries:
+                    dist = self.getMazeDistance(i, current_position)
+                    if dist < boundary_distance:
+                            boundary_distance = dist
+                            boundary_position = i
+            return boundary_position
+
+    
+    
+    
